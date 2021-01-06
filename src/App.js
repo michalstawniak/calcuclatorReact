15a095be4451f6e81fb4244a0d23f87e20d485a8
cube-animation <m.stawniakk@gmail.com>
2021-01-06 16:46:06 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,45 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  function handleButtonClick(e) {
-    console.log(e);
-  }
-  return (
-    <div class="calculator">
-        <div class="result">
-            <p></p>
-        </div>
+class App extends React.Component {
 
-        <span class="clear grey" >AC</span>
-        <span data-value={'+/-'} onClick={handleButtonClick} class="sign1 grey"><a class="sign"><sup>+</sup>/<sub>-</sub></a></span>
-        <span data-value={'%'} onClick={handleButtonClick} class="grey">%</span>
-        <span data-value={'÷'} onClick={handleButtonClick} class="operator">÷</span>
+    constructor(props) {
+        super(props);
+        this.state = {
+            result: ""
+        }
+        this.handleButtonClick = this.handleButtonClick.bind(this);
+    }
 
-        <span data-value={7} onClick={handleButtonClick}>7</span>
-        <span data-value={8} onClick={handleButtonClick}>8</span>
-        <span data-value={9} onClick={handleButtonClick}>9</span>
-        <span data-value={'X'} onClick={handleButtonClick} class="operator">x</span>
+    handleButtonClick(e) {
+        this.setState({
+            result: this.state.result + e.target.dataset.value
+        });
+    }
 
-        <span data-value={4} onClick={handleButtonClick}>4</span>
-        <span data-value={5} onClick={handleButtonClick(5)}>5</span>
-        <span data-value={6} onClick={handleButtonClick}>6</span>
-        <span  data-value={'-'} onClick={handleButtonClick}class="operator">-</span>
+    render() {
+        return (
+            <div className="calculator">
+                <div className="result">
+                    <p>{this.state.result}</p>
+                </div>
+                <span className="clear grey">AC</span>
+                <span data-value={'+/-'} onClick={this.handleButtonClick} className="sign1 grey"><a
+                    className="sign"><sup>+</sup>/<sub>-</sub></a></span>
+                <span data-value={'%'} onClick={this.handleButtonClick} className="grey">%</span>
+                <span data-value={'÷'} onClick={this.handleButtonClick} className="operator">÷</span>
 
-        <span data-value={1} onClick={handleButtonClick}>1</span>
-        <span data-value={2} onClick={handleButtonClick}>2</span>
-        <span data-value={3} onClick={handleButtonClick}>3</span>
-        <span data-value={'+'} onClick={handleButtonClick} class="operator">+</span>
+                <span data-value={7} onClick={this.handleButtonClick}>7</span>
+                <span data-value={8} onClick={this.handleButtonClick}>8</span>
+                <span data-value={9} onClick={this.handleButtonClick}>9</span>
+                <span data-value={'X'} onClick={this.handleButtonClick} className="operator">x</span>
 
-        <span data-value={0} onClick={handleButtonClick} class="double">0</span>
-        <span>.</span>
-        <span  data-value={'='} onClick={handleButtonClick}class="operator">=</span>
+                <span data-value={4} onClick={this.handleButtonClick}>4</span>
+                <span data-value={5} onClick={this.handleButtonClick}>5</span>
+                <span data-value={6} onClick={this.handleButtonClick}>6</span>
+                <span data-value={'-'} onClick={this.handleButtonClick} className="operator">-</span>
 
-    </div>
+                <span data-value={1} onClick={this.handleButtonClick}>1</span>
+                <span data-value={2} onClick={this.handleButtonClick}>2</span>
+                <span data-value={3} onClick={this.handleButtonClick}>3</span>
+                <span data-value={'+'} onClick={this.handleButtonClick} className="operator">+</span>
 
+                <span data-value={0} onClick={this.handleButtonClick} className="double">0</span>
+                <span data-value={'.'} onClick={this.handleButtonClick}>.</span>
+                <span data-value={'='} onClick={this.handleButtonClick} classNameName="operator">=</span>
 
-    
-  );
+            </div>
+        )
+    };
 }
-
 export default App;
