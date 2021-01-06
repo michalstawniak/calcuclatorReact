0e73b79f528f8bdf50a3355b662a597a978e3fc7
cube-animation <m.stawniakk@gmail.com>
2021-01-06 11:40:48 +0100

    new

--- src/App.js
+++ src/App.js
@@ -2,24 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div class = "calculator" >
+        <
+        div class = "result" >
+        <
+        p > < /p> <
+        /div>
+
+        <
+        span class = "clear grey" > AC < /span> <
+        span class = "sign1 grey" > < a class = "sign" > < sup > + < /sup>/ < sub > - < /sub></a > < /span> <
+        span class = "grey" > % < /span> <
+        span class = "operator" > ÷ < /span>
+
+        <
+        span > 7 < /span> <
+        span > 8 < /span> <
+        span > 9 < /span> <
+        span class = "operator" > x < /span>
+
+        <
+        span > 4 < /span> <
+        span > 5 < /span> <
+        span > 6 < /span> <
+        span class = "operator" > - < /span>
+
+        <
+        span > 1 < /span> <
+        span > 2 < /span> <
+        span > 3 < /span> <
+        span class = "operator" > + < /span>
+
+        <
+        span class = "double" > 0 < /span> <
+        span > . < /span> <
+        span class = "operator" >= < /span>
+
+        <
+        /div>
+
+    );
 }
 
 export default App;
