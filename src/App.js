1f408ba48198ba11ec1f23935e2891fa76649fdd
cube-animation <m.stawniakk@gmail.com>
2021-01-11 21:56:59 +0100

    new

--- src/App.js
+++ src/App.js
@@ -3,26 +3,18 @@ import React, {useState} from 'react'
 
 function App() {
 
-    const [state, update] = useState("")
+    const [state, setState] = useState("")
 
-    function Number(props) {
-        return (
-            <span data-value={props.value} onClick={handleButtonClick}>{props.value}</span>
-        )
-    }
-
-    function handleButtonClick(e) {
-        console.log(e.target.dataset.value);
-        let temp = state + e.target.dataset.value;
-        update(temp);
-    }
-
-    function deleteButtonClick() {
-        let temp = "";
-        update(temp);
+    const handleButtonClick = (e) => {
+        setState(state => state + e.target.dataset.value);
     }
 
     const numbers = [7,8,9,4,5,6,1,2,3,];
+
+    const listNumbers = numbers.map((numbers) => (
+            <span data-value={numbers} onClick={handleButtonClick}>{numbers}</span>
+            )
+        );
 
     return (
         <div className="calculator">
@@ -30,40 +22,26 @@ function App() {
                 <p>{state}</p>
             </div>
 
-            <span onClick={deleteButtonClick} className="clear grey">AC</span>
+            <span onClick={() => setState("")} className="clear grey">AC</span>
             <span data-value={'+/-'} onClick={handleButtonClick} className="grey">+/-</span>
             <span data-value={'%'} onClick={handleButtonClick} className="grey">%</span>
             <span data-value={'÷'} onClick={handleButtonClick} className="operator">÷</span>
-            <div className="numbers">{
-                numbers.map((numbers) => (
-                        <Number
-                            value={numbers}
-                        />
-                    )
-                )
-            }</div>
+
+            <div className="numbers">{listNumbers}</div>
             
             <div className="operators">
-            {/* <Number value="7" />
-            <Number value="8" />
-            <Number value="9" /> */}
             <span data-value={'X'} onClick={handleButtonClick} className="operator">x</span>
-
-            {/* <Number value="4" />
-            <Number value="5" />
-            <Number value="6" /> */}
             <span data-value={'-'} onClick={handleButtonClick} className="operator">-</span>
-
-            {/* <Number value="1" />
-            <Number value="2" />
-            <Number value="3" /> */}
             <span data-value={'+'} onClick={handleButtonClick} className="operator">+</span>
             </div>
+
             <span data-value={0} onClick={handleButtonClick} className="double">0</span>
             <span data-value={'.'} onClick={handleButtonClick}>.</span>
             <span data-value={'='} onClick={handleButtonClick} classNameName="operator">=</span>
 
         </div>
+
+
     );
 }
 
